02ec9a6c04905ba7885e070b53f9f17fdec5f5b7
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-06-30 19:37:09 -0300

    Atualização 30/06

--- php/update_user.php
+++ php/update_user.php
@@ -1,4 +1,8 @@
 <?php
+// CAPTURA O VALOR NA URL DO SITE
+$id = $_GET['codigo'];
+
+// INCLUINDO O ARQUIVO DE CONEXÃO 
 include 'conexao.php';
 
 // INSTRUÇÃO DO SQL PARA CAPTURAR USUARIO DIGITADO NA TELA DE LOGIN
@@ -9,6 +13,23 @@ $query = $conexao->query($select);
 
 // ARMAZENA A 1ª LINHA DO BD DENTRO DA VARIAVEL RESULTADO 
 $resultado = $query->fetch_assoc();
+
+// VERIFICA SE O FORMULÁRIO FOI ENVIADO
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $setor = $_POST['setor'];
+
+    // INSTRUÇÃO SQL PARA FAZER O UPDATE
+    $update = "UPDATE tb_user SET nm_user = '$nome', email = '$email', setor = '$setor' WHERE id_user = '$id'";
+
+    // EXECUTA O UPDATE
+    if ($conexao->query($update) === TRUE) {
+        echo "<script>alert('Usuário atualizado com sucesso!'); window.location.href='listar_usuario.php';</script>";
+    } else {
+        echo "Erro ao atualizar: " . $conexao->error;
+    }
+}
 ?>
 
 
@@ -33,12 +54,12 @@ $resultado = $query->fetch_assoc();
             <img src="images/deadpool.png" alt="imagem">
         </aside>
         <!-- Action manda pro php -->
-        <form action="php/insert_user.php" method="post">
+        <form action="#" method="post">
                 <label for="nome">NOME</label>
                 <input type="text" name="nome" value="<?php echo $resultado['nm_user']; ?>" id="nome">
                 
                 <label for="email">EMAIL</label>
-                <input type="text" name="email" value="<?php echo $resultado['email']; ?>"" id="email">
+                <input type="text" name="email" value="<?php echo $resultado['email']; ?>" id="email">
 
                 <label for="setor">SETOR</label>
                 <input type="text" name="setor" value="<?php echo $resultado['setor']; ?>" id="setor">
